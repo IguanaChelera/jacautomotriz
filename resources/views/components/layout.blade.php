a5935ec2b0ee33f93cc3aefc8b4d693187d9f8ef
iguano <ivanflash6@gmail.com>
2025-05-02 22:33:21 -0600

    Secure_asset

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,12 +6,12 @@
     <title>Jac Automotriz</title>
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ URL::asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}" />
     <!-- DataTables CSS -->
-    <link rel="stylesheet" href="{{ URL::asset('DataTables/datatables.min.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('DataTables/datatables.min.css') }}" />
     <!-- Tu CSS -->
-    <link rel="stylesheet" href="{{ URL::asset('assets/styles.css') }}" />
-    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/styles.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('bootstrap-5.3.3-dist/css/bootstrap-icons.css') }}">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -44,9 +44,9 @@
     </div>
 
     <!-- Bootstrap JS -->
-    <script src="{{ URL::asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}"></script>
     <!-- DataTables JS -->
-    <script src="{{ URL::asset('DataTables/datatables.min.js') }}"></script>
+    <script src="{{ secure_asset('DataTables/datatables.min.js') }}"></script>
     @yield('scripts')
 
 </body>
